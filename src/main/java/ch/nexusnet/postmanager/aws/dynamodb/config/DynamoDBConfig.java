5d1b84f1b4ec30a1b12e248c35782840d47870f6
Danielgergely <daniel.gergely@uzh.ch>
2024-04-05 13:43:04 +0200

    config(dynamoDB) added CORS settings to dynamoDB config file

--- src/main/java/ch/nexusnet/postmanager/aws/dynamodb/config/DynamoDBConfig.java
+++ src/main/java/ch/nexusnet/postmanager/aws/dynamodb/config/DynamoDBConfig.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableDynamoDBRepositories
@@ -41,4 +44,16 @@ public class DynamoDBConfig {
         return new BasicAWSCredentials(
                 amazonAWSAccessKey, amazonAWSSecretKey);
     }
+
+    // CORS Configuration
+    @Configuration
+    @EnableWebMvc
+    public class WebConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**");
+        }
+    }
+
 }
