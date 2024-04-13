3519d6f137f09708145c91608d413d4beb7c9cf3
Danielgergely <daniel.gergely@uzh.ch>
2024-04-13 15:29:49 +0200

    bugfix(cors): fixed missing closing parenthesis

--- src/main/java/ch/nexusnet/postmanager/aws/dynamodb/config/DynamoDBConfig.java
+++ src/main/java/ch/nexusnet/postmanager/aws/dynamodb/config/DynamoDBConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableDynamoDBRepositories
@@ -54,4 +55,5 @@ public class DynamoDBConfig {
             registry.addMapping("/**");
         }
     }
+}
 
